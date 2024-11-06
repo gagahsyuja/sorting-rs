0d284048cc246d1051869b69f55ce6d81946dde7
gagahsyuja <gghssa1@gmail.com>
2024-11-06 20:55:47 +0700

    Add test

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,11 +1,11 @@
-use sorting_rs::bubble::bubble;
+use sorting_rs::bubble_sort;
 
 fn main() 
 {
     let mut vec = vec![4, 7, 3, 6, 2, 7, 2, 8, 1, 9, 2];
 
     // let sort = Bubble::new(vec);
-    bubble(&mut vec);
+    bubble_sort(&mut vec);
 
     println!("{:#?}", vec);
 }
