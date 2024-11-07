35eaf212f0cc1bc5954f93e26e6ddda4379393d3
gagahsyuja <gghssa1@gmail.com>
2024-11-07 14:36:35 +0700

    Add insertion sort test, rewrite test

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,12 +1,17 @@
-use sorting_rs::{ bubble_sort, selection_sort };
+use sorting_rs::{ bubble_sort, selection_sort, insertion_sort };
 
 fn main() 
 {
     let mut vec = vec![4, 7, 3, 6, 2, 7, 2, 8, 1, 9, 2];
 
     bubble_sort(&mut vec);
-    selection_sort(&mut vec);
-
     println!("Bubble\t\t: {:?}", vec);
+
+    vec = vec![4, 7, 3, 6, 2, 7, 2, 8, 1, 9, 2];
+    selection_sort(&mut vec);
     println!("Selection\t: {:?}", vec);
+
+    vec = vec![4, 7, 3, 6, 2, 7, 2, 8, 1, 9, 2];
+    insertion_sort(&mut vec);
+    println!("Insertion\t: {:?}", vec);
 }
